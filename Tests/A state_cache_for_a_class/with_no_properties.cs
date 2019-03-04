5dff186e8a945d7a67a08b7af64921111434dc89
mbreckon <matt@breckonworld.com>
2019-03-04 22:28:14 +0000

    Add tests for class with no properties

---
+++ Tests/A state_cache_for_a_class/with_no_properties.cs
@@ -0,0 +1,37 @@
+﻿using StateCache;
+using TestClasses;
+using Xunit;
+
+namespace A_state_cache_for_a_class
+{
+   public class with_no_properties
+   {
+      [Fact]
+      public void builds_when_no_properties_are_requested()
+      {
+         new StateCacheBuilder<ClassWithNoProperties>()
+            .Build();
+      }
+      
+      [Fact]
+      public void doesnt_crash_when_asked_to_store()
+      {
+         new StateCacheBuilder<ClassWithNoProperties>()
+            .Build()
+            .Store(new ClassWithNoProperties());
+      }
+
+      [Fact]
+      public void doesnt_crash_when_asked_to_restore()
+      {
+         var stateCache =
+            new StateCacheBuilder<ClassWithNoProperties>()
+               .Build();
+
+         var state = new ClassWithNoProperties();
+
+         stateCache.Store(state);
+         stateCache.Restore(state);
+      }
+   }
+}
---
+++ Tests/TestClasses/ClassWithNoProperties.cs
@@ -0,0 +1,4 @@
+﻿namespace TestClasses
+{
+   public class ClassWithNoProperties { }
+}
