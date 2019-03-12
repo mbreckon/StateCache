57efeb3373a2ec9868f8034c79901b261f84f848
mbreckon <matt@breckonworld.com>
2019-03-12 20:04:46 +0000

    Add tests for public class only comprising methods

--- Tests/A state_cache_for_a_class/with_only_methods.cs
+++ Tests/A state_cache_for_a_class/with_only_methods.cs
@@ -1,34 +1,34 @@
 ﻿using StateCache;
-using TestClasses;
+using Tests.TestClasses;
 using Xunit;
 
 namespace A_state_cache_for_a_public_class
 {
-   public class with_no_properties
+   public class with_only_methods
    {
       [Fact]
       public void builds_when_no_properties_are_requested()
       {
-         new StateCacheBuilder<PublicClassWithNoProperties>()
+         new StateCacheBuilder<PublicClassWithOnlyMethods>()
             .Build();
       }
-      
+
       [Fact]
       public void doesnt_crash_when_asked_to_store()
       {
-         new StateCacheBuilder<PublicClassWithNoProperties>()
+         new StateCacheBuilder<PublicClassWithOnlyMethods>()
             .Build()
-            .Store(new PublicClassWithNoProperties());
+            .Store(new PublicClassWithOnlyMethods());
       }
 
       [Fact]
       public void doesnt_crash_when_asked_to_restore()
       {
          var stateCache =
-            new StateCacheBuilder<PublicClassWithNoProperties>()
+            new StateCacheBuilder<PublicClassWithOnlyMethods>()
                .Build();
 
-         var state = new PublicClassWithNoProperties();
+         var state = new PublicClassWithOnlyMethods();
 
          stateCache.Store(state);
          stateCache.Restore(state);
---
+++ Tests/TestClasses/PublicClassWithOnlyMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace Tests.TestClasses
+{
+   public class PublicClassWithOnlyMethods
+   {
+      public void APublicMethod() { }
+      public int APublicMethodReturningAnInt() { return 1; }
+      protected void AProtectedMethod() { }
+      private void APrivateMethod() { }
+   }
+}
